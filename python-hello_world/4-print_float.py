a8c3ed6ac64d8a8d94f7dd1f6e84801275fec8c7
Eziknmad <galarzaarzon@gmail.com>
2026-01-16 20:26:29 -0400

    edit the code 5th time

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-number = 3.14159265359
+
+number = 3.14159265359  # or try "3.14" (string) for error case
+
 
 try:
     print(f"Float: {number:.2f}")
-except ValueError as e:
-    print(e)
+except (ValueError, TypeError) as e:
+    print("ValueError: " + str(e))
