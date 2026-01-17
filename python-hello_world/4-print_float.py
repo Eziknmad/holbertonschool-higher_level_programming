f8adb8b0094ae2173e9d3a111fefbe4f4b395dfd
Eziknmad <galarzaarzon@gmail.com>
2026-01-16 20:40:34 -0400

    still trying to find the right compination

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
-
-number = 3.14159265359  # or try "3.14" (string) for error case
-
-
-try:
-    print(f"Float: {number:.2f}")
-except (ValueError, TypeError) as e:
-    print("ValueError: " + str(e))
+number = 3.14159
+print(f"Float: {number:.2f}")
