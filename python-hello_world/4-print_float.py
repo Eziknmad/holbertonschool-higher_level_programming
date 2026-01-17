6a23afb78a5f3ef9972dce6fd6232e52a930d270
Eziknmad <galarzaarzon@gmail.com>
2026-01-16 20:08:39 -0400

    3rd try with pi program

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-number = "3.14159265358979323846264338327950288419716939937510582097494459"
-number = float(number)  # convert string to float
-
+number = 3.14159265359
 print(f"Float: {number:.2f}")
