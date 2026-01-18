031b50822fdf88454549aa80d580a2606df5e753
Eziknmad <galarzaarzon@gmail.com>
2026-01-18 00:16:33 -0400

    remove uppercase file

--- python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{:c}".format(ord(c)), end="")
-    print()
