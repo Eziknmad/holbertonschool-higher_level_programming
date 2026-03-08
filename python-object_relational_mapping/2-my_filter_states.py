68a75f1ee1a84ffa39ea32efc385fed1f8c722b0
Eziknmad <galarzaarzon@gmail.com>
2026-03-08 16:17:21 -0400

    Add script to filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,9 +23,8 @@ if __name__ == "__main__":
     )
 
     cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        state_name)
-    cur.execute(query)
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
+    cur.execute(query.format(state_name))
 
     rows = cur.fetchall()
     for row in rows:
