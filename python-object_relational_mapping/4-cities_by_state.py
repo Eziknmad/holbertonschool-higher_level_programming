f9df58f924131b53e3caabb7e3be0524b2738851
Eziknmad <galarzaarzon@gmail.com>
2026-03-08 16:30:15 -0400

    Add script to list all cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -21,8 +21,10 @@ if __name__ == "__main__":
     )
 
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
-                "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                   FROM cities
+                   INNER JOIN states ON cities.state_id = states.id
+                   ORDER BY cities.id ASC""")
 
     rows = cur.fetchall()
     for row in rows:
