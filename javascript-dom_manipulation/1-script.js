f83f82236686438077dd2b68966d5c6496f457dc
Eziknmad <galarzaarzon@gmail.com>
2026-03-24 06:10:57 -0400

    feat: add click event to change header color to red
    - Implement event listener on #red_header div
    - Update header text color to #FF0000 on click
    - Use querySelector for DOM element selection
    - Follow semistandard style guidelines
    - No var keyword used, no page reload required
    
    Task: 1. Click and turn red
    File: javascript-dom_manipulation/1-script.js

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+// Select the div with id 'red_header' and add click event listener
+document.querySelector('#red_header').addEventListener('click', function () {
+  // When clicked, change the header text color to red
+  document.querySelector('header').style.color = '#FF0000';
+});
